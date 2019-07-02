1a1e6fe0038f5bbbfbd82a1fe58f3f7cd7ed2d4b
ricardo suzumura <ricardo.suzumura@fatec.sp.gov.br>
2019-07-01 22:20:09 -0300

    Changes to support .NET Framework 4.6.1

--- src/AngleSharp.Io.Tests/Properties/AssemblyInfo.cs
+++ src/AngleSharp.Io.Tests/Properties/AssemblyInfo.cs
@@ -1,8 +1,20 @@
-﻿using System.Reflection;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCopyright("Copyright © AngleSharp, 2013-2019.")]
+[assembly: AssemblyTitle("AngleSharp.Io.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AngleSharp.Io.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
 [assembly: ComVisible(false)]
-[assembly: Guid("863a9ca5-a39b-42a9-967f-301d5fb8e630")]
+
+[assembly: Guid("6b4c895c-87e7-428e-8575-88ad3442d8b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/AngleSharp.Io/Properties/AssemblyInfo.cs
+++ src/AngleSharp.Io/Properties/AssemblyInfo.cs
@@ -2,6 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCopyright("Copyright © AngleSharp, 2013-2019")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AngleSharp.Io")]
+[assembly: AssemblyDescription("Providers additional requesters and IO helpers for AngleSharp.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AngleSharp.Io")]
+[assembly: AssemblyProduct("AngleSharp.Io")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleToAttribute("AngleSharp.Io.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010001adf274fa2b375134e8e4558d606f1a0f96f5cd0c6b99970f7cce9887477209d7c29f814e2508d8bd2526e99e8cd273bd1158a3984f1ea74830ec5329a77c6ff201a15edeb8b36ab046abd1bce211fe8dbb076d7d806f46b15bfda44def04ead0669971e96c5f666c9eda677f28824fff7aa90d32929ed91d529a7a41699893")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b4eb481-c849-46d7-ab05-2d73a5490fdb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.12.1.0")]
+[assembly: AssemblyFileVersion("0.12.1.0")]
